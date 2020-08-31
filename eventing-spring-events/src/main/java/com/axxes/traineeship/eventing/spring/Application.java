b7fe0b92e9169799010c31eaaa4de9dcfb3e030a
PieterDr <pieter.drijkoningen@vrt.be>
2020-08-31 21:38:10 +0200

    Adapt Spring events exercise to 2020 project

--- eventing-spring-events/src/main/java/com/axxes/traineeship/eventing/spring/Application.java
+++ eventing-spring-events/src/main/java/com/axxes/traineeship/eventing/spring/Application.java
@@ -1,25 +1,63 @@
 package com.axxes.traineeship.eventing.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @SpringBootApplication
-@EnableScheduling
 public class Application {
 
+    /*
+     * Build an event-driven service, which could serve as the backend for the Traineeship project
+     *
+     * This service should have following features:
+     *  - The audience can submit questions during a specific talk on which other audience members can vote
+     *  - The speaker can initiate a poll in which the audience can partake
+     *
+     * Hints:
+     *  - For publishing events -> use the ApplicationEventPublisher bean provided by Spring
+     *  - For consuming events -> use the @EventListener annotation, e.g.
+     *
+     *        @EventListener
+     *        public void consume(SomeEvent event) { ... }
+     *
+     */
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        // We're going to build an event-driven service, which could serve as the back-end for the project case
-        // This service should have following features:
-        //  - A REST API allowing consultants to submit check-ins
-        //  - For each submitted check-in, the consultant and coach are notified by mail
-        //  - Sending out reminders to consultants to submit their quarterly check-in.
-        // Hints:
-        //  - For publishing events -> use the ApplicationEventPublisher bean provided by Spring
-        //  - For consuming events -> use the @EventListener annotation, e.g.
-        //      @EventListener
-        //      public void consume(SomeEvent event) {}
+        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+        Api api = ctx.getBean(Api.class);
+    }
+
+    @Component
+    static class Api {
+
+        private final ApplicationEventPublisher eventPublisher;
+
+        @Autowired
+        Api(ApplicationEventPublisher eventPublisher) {
+            this.eventPublisher = eventPublisher;
+        }
+
+        public void submitQuestion(int talkId, String question) {
+
+        }
+
+        public void voteOnQuestion(int talkId, int questionId) {
+
+        }
+
+        public void submitPoll(String question, List<String> answers) {
+
+        }
+
+        public void answerPoll(int pollId, int answer) {
+
+        }
+
     }
 
 }
